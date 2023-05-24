da00f64e76e1fc030e0447fc7de9d75a8c2fa937
Kristin Galvin <kgalvin@nvidia.com>
2023-05-24 11:05:29 -0700

    add interpretation algorithms

--- src/utils/matrixInterpreter.ts
+++ src/utils/matrixInterpreter.ts
@@ -1,29 +1,33 @@
-import { DataOptionType } from "@/utils/matrixGenerator";
-import { rotateCw90 } from "./matrixTransformer";
+import {DataOptionType} from "@/utils/matrixGenerator";
+import {rotateCw90} from "./matrixTransformer";
 
 export interface InterpretationMatrix {
   matrix: number[][];
   interpretation: string;
-  options?: {
-
-  }
+  options?: {}
 }
 
 export const interpretations: DataOptionType[] = [
-  { value: "none", label: "none" },
-  { value: "averageRows", label: "1D: Average Rows" },
-  { value: "averageColumns", label: "1D: Average Columns" },
-  { value: "twoDimensions", label: "2D to 2D" },
-  { value: "otherDimensions", label: "2D to ND" },
+  {value: "none", label: "none"},
+  {value: "averageRows", label: "1D: Average Rows"},
+  {value: "averageColumns", label: "1D: Average Columns"},
+  {value: "sumRows", label: "1D: Sum Rows Mod(1)"},
+  {value: "sumColumns", label: "1D: Sum Columns Mod(1)"},
+  {value: "twoDimensions", label: "2D to 2D"},
+  {value: "otherDimensions", label: "2D to ND"},
 ];
 
-export const interpretMatrix = ({ interpretation, matrix, options }: InterpretationMatrix): number[] | number[][] => {
+export const interpretMatrix = ({interpretation, matrix, options}: InterpretationMatrix): number[] | number[][] => {
   switch (interpretation) {
     case 'none':
       return matrix;
     case 'averageRows':
       return averageRows(matrix);
     case 'averageColumns':
+      return averageColumns(matrix);
+    case 'sumRows':
+      return averageRows(matrix);
+    case 'sumColumns':
       return averageColumns(matrix);
     default:
       return matrix;
@@ -37,4 +41,13 @@ const averageRows = (matrix: number[][]): number[] => {
 const averageColumns = (matrix: number[][]): number[] => {
   const rotatedMatrix = rotateCw90(matrix);
   return rotatedMatrix.map((row) => row.reduce((a, b) => a + b, 0) / row.length);
+}
+
+const sumRows = (matrix: number[][]): number[] => {
+  return matrix.map((row) => row.reduce((a, b) => a + b, 0) % 1);
+}
+
+const sumColumns = (matrix: number[][]): number[] => {
+  const rotatedMatrix = rotateCw90(matrix);
+  return rotatedMatrix.map((row) => row.reduce((a, b) => a + b, 0) % 1);
 }
--- src/utils/matrixTransformer.ts
+++ src/utils/matrixTransformer.ts
@@ -1,26 +1,24 @@
-import { DataOptionType } from "@/utils/matrixGenerator";
+import {DataOptionType} from "@/utils/matrixGenerator";
 
 export interface TransformMatrix {
   matrix: number[][];
   transform: string;
-  options?: {
-
-  }
+  options?: {}
 }
 
 export const transforms: DataOptionType[] = [
-  { value: "none", label: "None" },
-  { value: "invert", label: "Invert" },
-  { value: "flipX", label: "Flip X" },
-  { value: "flipY", label: "Flip Y" },
-  { value: "rotate180", label: "Rotate 180" },
-  { value: "rotateCw90", label: "Rotate CW 90" },
-  { value: "log", label: "Log" },
-  { value: "exponent", label: "Exponent" },
-  { value: "modulo", label: "Modulo(%) 0.25" },
+  {value: "none", label: "None"},
+  {value: "invert", label: "Invert"},
+  {value: "flipX", label: "Flip X"},
+  {value: "flipY", label: "Flip Y"},
+  {value: "rotate180", label: "Rotate 180"},
+  {value: "rotateCw90", label: "Rotate CW 90"},
+  {value: "log", label: "Log-ish (1 - 1/(1+x)) * 2"},
+  {value: "exponent", label: "Exponent"},
+  {value: "modulo", label: "Modulo(%) 0.25"},
 ];
 
-export const transformMatrix = ({ transform, matrix, options }: TransformMatrix): number[][] => {
+export const transformMatrix = ({transform, matrix, options}: TransformMatrix): number[][] => {
   switch (transform) {
     case 'none':
       return matrix;
@@ -58,7 +56,7 @@ const flipX = (matrix: number[][]): number[][] => {
 }
 
 const flipY = (matrix: number[][]): number[][] => {
- return  matrix.map(row=>row).reverse()
+  return matrix.map(row => row).reverse()
 }
 
 const rotate180 = (matrix: number[][]): number[][] => {
@@ -66,26 +64,26 @@ const rotate180 = (matrix: number[][]): number[][] => {
 }
 
 const modulo = (matrix: number[][], modNum: number): number[][] => {
-  return matrix.map((row) => row.map((point) => point%modNum));
+  return matrix.map(row => row.map(point => (point % modNum) * 1 / modNum));
 }
 
 const log = (matrix: number[][]): number[][] => {
-  return matrix.map((row) => row.map((point) => point !== 0 ? Math.log(point) : 0));
+  return matrix.map(row => row.map(point => (1 - 1 / (1 + point)) * 2));
 }
 
 const exponent = (matrix: number[][]): number[][] => {
-  return matrix.map((row) => row.map((point) => (point * point)));
+  return matrix.map(row => row.map(point => (point * point)));
 }
 
 export const rotateCw90 = (matrix: number[][]): number[][] => {
   const width = matrix.length;
-	const height = matrix[0].length;
-	let temp = new Array(height);
-	for (let y=0; y<height; y++) {
-		temp[y] = new Array(width);
-		for (let x=0; x<width; x++) {
-			temp[y][x] = matrix[width-1-x][y];
-		}
-	}
-	return temp;
+  const height = matrix[0].length;
+  let temp = new Array(height);
+  for (let y = 0; y < height; y++) {
+    temp[y] = new Array(width);
+    for (let x = 0; x < width; x++) {
+      temp[y][x] = matrix[width - 1 - x][y];
+    }
+  }
+  return temp;
 }
