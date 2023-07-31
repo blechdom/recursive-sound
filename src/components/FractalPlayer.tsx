79d09aac4b799b4dafec0c21372284afb8a8447f
kgalvin <kgalvin@nvidia.com>
2023-07-30 17:32:14 -0700

    trying to fix difference bug

--- src/components/FractalPlayer.tsx
+++ src/components/FractalPlayer.tsx
@@ -9,19 +9,15 @@ import {Label} from "@/pages/fractalPlayheads";
 import React, {useCallback, useEffect, useRef, useState} from "react";
 import io, {Socket} from "socket.io-client";
 import styled from "styled-components";
-import Select from "react-select";
 import {
-  colourPalettes,
   defaultJuliaPlane,
   defaultMandelbrotPlane,
   drawPlayhead,
   generateFractal,
   FractalPlane,
   getScalingFactors,
-  OptionType,
   rotateMatrixCW90,
   clearCanvas,
-  transformAudioMatrix,
 } from "@/utils/fractalGenerator";
 
 let socket: Socket;
@@ -37,42 +33,36 @@ type FractalPlayerProps = {
 const FractalPlayer: React.FC<FractalPlayerProps> = ({fractal, cx = -0.7, cy = 0.27015, setCx, setCy}) => {
   const maxIterations = 100;
   const lsmThreshold = 100;
-  const demThreshold = 0.2;
-  const overflow = 100000000000;
-  const demColorModulo = 100;
 
   const plane: FractalPlane = fractal === 'mandelbrot' ? defaultMandelbrotPlane : defaultJuliaPlane;
   const [size, setSize] = useState<number>(256);
+  const [program, setProgram] = useState<string>('lsm-binary');
 
   const [mandelbrotMouseDown, setMandelbrotMouseDown] = useState<boolean>(false);
 
   const [fractalWindow, setFractalWindow] = useState<FractalPlane>(plane)
-  const [fractal2DArray, setFractal2DArray] = useState<number[][]>([]);
-  const [audio2DArray, setAudio2DArray] = useState<number[][]>([]);
-  const [audioTransformed, setAudioTransformed] = useState<number[][]>([]);
-  const [fractalTransformed, setFractalTransformed] = useState<number[][]>([]);
+  const [rawFractalData, setRawFractalData] = useState<number[][]>([]);
+  const [audioFractalData, setAudioFractalData] = useState<number[][]>([]);
+  const [playheadFractalData, setPlayheadFractalData] = useState<number[][]>([]);
+
   const [fractalSpeed, setFractalSpeed] = useState<number>(50);
   const [fractalVolume, setFractalVolume] = useState<number>(0);
   const [fractalThreshold, setFractalThreshold] = useState<number>(0);
   const [fractalInterval, setFractalInterval] = useState<number>(0);
+
   const [fractalPlayheadType, setFractalPlayheadType] = useState<string>('down');
   const [fractalTransport, setFractalTransport] = useState<string>('stop');
   const [fractalTimeouts, setFractalTimeouts] = useState<any[]>([]);
   const [fractalPauseTimeElapsed, setFractalPauseTimeElapsed] = useState<number>(0);
   const [fractalLoop, setFractalLoop] = useState<boolean>(false);
-  const [program, setProgram] = useState<string>('lsm-binary');
+
   const fractalCanvasRef = useRef<HTMLCanvasElement | null>(null);
   const fractalPlayheadCanvasRef = useRef<HTMLCanvasElement | null>(null);
 
 
   const socketInitializer = async () => {
     await fetch("/recursive-sound/api/socket");
-
     socket = io();
-
-    socket.on("newIncomingMessage", (msg) => {
-      console.log(msg);
-    });
   };
 
   useEffect(() => {
@@ -81,24 +71,18 @@ const FractalPlayer: React.FC<FractalPlayerProps> = ({fractal, cx = -0.7, cy = 0
 
   useEffect(() => {
     if (fractalPlayheadType === 'left' || fractalPlayheadType === 'right') {
-      setFractalTransformed(rotateMatrixCW90(audioTransformed));
+      setPlayheadFractalData(rotateMatrixCW90(audioFractalData));
     } else {
-      setFractalTransformed(audioTransformed);
+      setPlayheadFractalData(audioFractalData);
     }
-  }, [fractalPlayheadType, audioTransformed]);
+  }, [fractalPlayheadType, audioFractalData]);
 
   useEffect(() => {
     setFractalTransport('stop');
-  }, [fractalPlayheadType]);
+  }, [fractalPlayheadType, fractalLoop, program]);
 
   useEffect(() => {
-    setFractalTransport('stop');
-  }, [fractalLoop]);
-
-  useEffect(() => {
-    let renderType = 'lsm';
-    if (program === 'lsm-raw') renderType = 'lsm-raw';
-    getFractal(renderType, 16);
+    getFractal();
   }, [cx, cy, size, fractalWindow, program]);
 
   useEffect(() => {
@@ -112,23 +96,9 @@ const FractalPlayer: React.FC<FractalPlayerProps> = ({fractal, cx = -0.7, cy = 0
       setFractalTransport('play');
     }
   }, [fractalTransport]);
-
-  useEffect(() => {
-    if (audio2DArray.length > 0) {
-      if (program === 'lsm-binary' || program === 'lsm-raw') {
-        setAudioTransformed(audio2DArray);
-      } else if (program === 'lsm-difference') {
-        setAudio2DArray(fractal2DArray);
-        setAudioTransformed(transformAudioMatrix(fractal2DArray, program));
-      } else {
-        setAudioTransformed(transformAudioMatrix(audio2DArray, program));
-      }
-    }
-  }, [audio2DArray, program]);
-
-  const getFractal = (renderType: string, paletteNumber: number) => {
+  
+  const getFractal = () => {
     if (fractalCanvasRef.current) {
-      const threshold = renderType === 'dem' ? demThreshold : lsmThreshold;
       let fractalArray: {
         fractalData: number[][],
         audioData: number[][],
@@ -141,18 +111,15 @@ const FractalPlayer: React.FC<FractalPlayerProps> = ({fractal, cx = -0.7, cy = 0
         fractalCanvasRef.current,
         fractalWindow,
         size,
-        size,
-        renderType,
+        program,
         maxIterations,
-        threshold,
+        lsmThreshold, // or demThreshold
         cx,
-        cy,
-        overflow,
-        demColorModulo,
-        paletteNumber
+        cy
       );
-      setFractal2DArray(fractalArray.fractalData);
-      setAudio2DArray(fractalArray.audioData);
+      setRawFractalData(fractalArray.fractalData);
+      setAudioFractalData(fractalArray.audioData);
+      setPlayheadFractalData(fractalArray.audioData);
     }
   };
 
@@ -160,7 +127,7 @@ const FractalPlayer: React.FC<FractalPlayerProps> = ({fractal, cx = -0.7, cy = 0
     fractalTimeouts.forEach(async (timeoutId) => {
       await clearTimeout(timeoutId);
     });
-    socket?.emit("fractalMandelbrotRow", fractalTransformed[0].fill(0));
+    socket?.emit("fractalMandelbrotRow", playheadFractalData[0].fill(0));
     if (fractalPlayheadCanvasRef.current) clearCanvas(fractalPlayheadCanvasRef.current);
     setFractalPauseTimeElapsed(0);
   }
@@ -173,17 +140,17 @@ const FractalPlayer: React.FC<FractalPlayerProps> = ({fractal, cx = -0.7, cy = 0
 
   const playFractal = async () => {
     const timeoutIds: any[] = [];
-    for (let i = 0; i < fractalTransformed.length; i++) {
+    for (let i = 0; i < playheadFractalData.length; i++) {
       let index = i;
       if (fractalPlayheadType === 'up' || fractalPlayheadType === 'left' || fractalPlayheadType === 'in' || fractalPlayheadType === 'ccw') {
-        index = fractalTransformed.length - 1 - i;
+        index = playheadFractalData.length - 1 - i;
       }
       const timeoutId = setTimeout(function () {
         if (fractalPlayheadCanvasRef.current) {
           drawPlayhead(fractalPlayheadCanvasRef.current, fractalPlayheadType, index);
         }
-        socket?.emit("fractalMandelbrotRow", fractalTransformed[index]);
-        if (i >= fractalTransformed.length - 1) {
+        socket?.emit("fractalMandelbrotRow", playheadFractalData[index]);
+        if (i >= playheadFractalData.length - 1) {
           if (fractalLoop) {
             setFractalTransport('replay');
           } else {
@@ -211,7 +178,7 @@ const FractalPlayer: React.FC<FractalPlayerProps> = ({fractal, cx = -0.7, cy = 0
         x: e.clientX - rect.left,
         y: e.clientY - rect.top
       };
-      const scalingFactors = getScalingFactors(fractalWindow, size, size);
+      const scalingFactors = getScalingFactors(fractalWindow, size);
       const newCx = fractalWindow.x_min + pos.x * scalingFactors.x;
       const newCy = fractalWindow.y_min + pos.y * scalingFactors.y;
       if (fractal == 'mandelbrot' && setCx && setCy) {
@@ -289,10 +256,9 @@ const FractalPlayer: React.FC<FractalPlayerProps> = ({fractal, cx = -0.7, cy = 0
           </CanvasContainer>
           <ControlRows>
             <ControlRow>
-              <PlayheadData title={"Fractal Data"} matrixData={fractal2DArray}/>
-              <PlayheadData title={"Audio Data"} matrixData={audio2DArray}/>
-              <PlayheadData title={"Transformed Data"} matrixData={audioTransformed}/>
-              <PlayheadData title={"Playhead Data"} matrixData={fractalTransformed}/>
+              <PlayheadData title={"Fractal Data"} matrixData={rawFractalData}/>
+              <PlayheadData title={"Audio Data"} matrixData={audioFractalData}/>
+              <PlayheadData title={"Playhead Data"} matrixData={playheadFractalData}/>
             </ControlRow>
           </ControlRows>
         </FractalContainer>
