ee074e0102f8247bcafee9b6d85242ad79c9b917
kgalvin <kgalvin@nvidia.com>
2023-08-14 20:40:52 -0700

    temp type fix

--- src/components/AudioEngine.tsx
+++ src/components/AudioEngine.tsx
@@ -33,39 +33,44 @@ const AudioEngine: React.FC<AudioEngineProps> = ({
   const [fftVizData, setFftVizData] = useState<any>();
 
   useEffect(() => {
-    const SignalSynth = (signal: NodeRepr_t) => {
-      if (playing && signal && core) {
-        let synth = el.mul(
-          signal,
-          el.sm(el.const({key: `main-amp`, value: volume}))
-        ) as NodeRepr_t;
-        synth = el.scope({name: `scope-${fractal}`}, synth);
-        synth = el.fft({name: `fft-${fractal}`}, synth);
-        core.render(synth, synth);
-      }
-    };
-
-    const Resynth = () => {
-      let accum = 0;
-      const allVoices = [...Array(fractalRow.length)].map((_, i) => {
-        const key = `osc-freq-${i}`;
-        accum += fractalRow[i];
-        const freq = el.sm(el.const({key, value: lowest * (2 ** ((i * interval) / 12))}));
-        return el.mul(el.cycle(freq), el.sm(el.const({key: `osc-amp-${i}`, value: fractalRow[i]})));
-      });
-
-      const addMany = (ins: NodeRepr_t[]): NodeRepr_t => {
-        return el.add(...ins) as NodeRepr_t;
-      };
-      const rowMult = accum !== 0 ? 1 / accum : 0;
-      console.log('rowMult', rowMult);
-      return el.mul(addMany(allVoices as NodeRepr_t[]), el.sm(el.const({
-        key: "row-gain",
-        value: rowMult
-      }))) as NodeRepr_t;
-    }
-
     if (audioContext) {
+      const SignalSynth = (signal: NodeRepr_t) => {
+        if (playing && signal && core) {
+          let synth = el.mul(
+            signal,
+            el.sm(el.const({key: `main-amp`, value: volume}))
+          ) as NodeRepr_t;
+          synth = el.scope({name: `scope-${fractal}`}, synth);
+          synth = el.fft({name: `fft-${fractal}`}, synth);
+          core.render(synth, synth);
+        }
+      };
+
+      const Resynth = () => {
+        let accum = 0;
+        const allVoices = [...Array(fractalRow.length)].map((_, i) => {
+          const key = `osc-freq-${i}`;
+          const freq = lowest * (2 ** ((i * interval) / 12));
+          if (freq < audioContext.sampleRate / 2) {
+            accum += fractalRow[i];
+            const freqSignal = el.sm(el.const({key, value: freq}));
+            return el.mul(el.cycle(freqSignal), el.sm(el.const({key: `osc-amp-${i}`, value: fractalRow[i]})));
+          } else {
+            return el.const({key, value: 0});
+          }
+        });
+
+        const addMany = (ins: NodeRepr_t[]): NodeRepr_t => {
+          return el.add(...ins) as NodeRepr_t;
+        };
+        const rowMult = accum !== 0 ? 1 / accum : 0;
+        console.log('rowMult', rowMult);
+        return el.mul(addMany(allVoices as NodeRepr_t[]), el.sm(el.const({
+          key: "row-gain",
+          value: rowMult
+        }))) as NodeRepr_t;
+      }
+
       if (playing) {
         audioContext.resume();
         SignalSynth(Resynth());
@@ -73,7 +78,6 @@ const AudioEngine: React.FC<AudioEngineProps> = ({
         audioContext.suspend();
       }
     }
-
   }, [playing, volume, threshold, interval, lowest, fractalRow, audioContext, core]);
 
   core?.on("scope", function (e) {
